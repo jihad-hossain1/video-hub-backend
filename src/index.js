ef6016388c2b372abc01927419105c0a5c85b26f
jihad-hossain1 <jihadkhan934@gmail.com>
2024-01-16 23:48:17 +0600

    apierro apirespose asynchandler handle on utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MOngo connection faild : ", error);
+  });
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
