8b98be5cadf71b65ee9cf89242b6986ec4ab1d32
jihad-hossain1 <jihadkhan934@gmail.com>
2024-01-16 22:49:58 +0600

    mongodb connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("erroR", (error) => {
+      console.log("erroR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+  }
+})();
+
+*/
